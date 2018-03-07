73ba08092ea5ce4c39cf251a0daee27f408cddb5
kenjisato97 <knjsato1@gmail.com>
2018-03-07 15:19:20 -0300

    roteiro 1

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main()
+{
+    	char palavra[50];
+	printf("digite a palavra");
+	scanf("%c", palavra);
+	printf("%c", palavra[1]);
+    
+
+return 0;
+}
