d1b4c4d78795cf9971806e44fc33fbbcbba83bb1
KenjiSato97 <30627646+KenjiSato97@users.noreply.github.com>
2018-04-23 15:27:21 -0300

    Update InvoiceTest.cpp

--- InvoiceTest.cpp
+++ InvoiceTest.cpp
@@ -3,7 +3,15 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
+    Invoice produto(1,"pillha AA",3,4.00);
+
+	cout << "numero: " << produto.getNumero()
+            <<  "  " << "produto:" << "  " << produto.getDescricao()
+            << "  " << "quantidade:" << "  " << produto.getQuant()
+            << "  " << "preco:" << "  " << produto.getPreco() << endl;
+
+
+	cout << "fatura: " << produto.getInvoiceAmount() << endl;
 
 }
