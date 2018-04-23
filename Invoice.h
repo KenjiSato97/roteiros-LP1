964958a37669a7818e9a6124cebc8bd018e47973
KenjiSato97 <30627646+KenjiSato97@users.noreply.github.com>
2018-04-23 15:26:52 -0300

    Update Invoice.h

--- Invoice.h
+++ Invoice.h
@@ -1,27 +1,28 @@
 #ifndef INVOICE_H_
 #define INVOICE_H_
 
+using namespace std;
 class Invoice {
 	private:
 	    int numero;
-	    char descricao;
+	    string descricao;
 	    int quantidade;
 	    float preco;
 	    float InvoiceAmount = quantidade*preco;
 
 	public:
-		Invoice(int, char, int, float);
+		Invoice(int, string, int, float);
 		Invoice();
 		virtual ~Invoice();
 		int getNumero();
-		char getDescricao();
+		string getDescricao();
 		int getQuant();
 		float getPreco();
 		void setNumero(int);
-		void setDescricao(char);
+		void setDescricao(string);
 		void setQuant(int);
 		void setPreco(float);
-		void getInvoiceAmount();
+		float getInvoiceAmount();
 
 
 };
