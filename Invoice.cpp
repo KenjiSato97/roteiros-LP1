bd9974f555d1dec91c37f04b3db4d2e948c954c0
KenjiSato97 <30627646+KenjiSato97@users.noreply.github.com>
2018-04-23 15:26:07 -0300

    Update Invoice.cpp

--- Invoice.cpp
+++ Invoice.cpp
@@ -5,59 +5,54 @@ using namespace std;
 
 Invoice::Invoice(){
 
+
 }
 
-Invoice::Invoice(int n, int m, int a){
+Invoice::Invoice(int n, string d, int q, float p){
 	numero = n;
 	descricao = d;
 	quantidade = q;
 	preco = p;
 
-
-	if (verificaData() == false){
-		cout << "Data invalida" << endl;
-		dia = 1;
-		mes = 1;
-		ano = 2000;
-	}
 }
 
-Data::~Data(){
+Invoice::~Invoice(){
 
 }
 
-int Data::getDia(){
-	return dia;
+int Invoice::getNumero(){
+	return numero;
 }
 
-int Data::getMes(){
-	return mes;
+string Invoice::getDescricao(){
+	return descricao;
 }
 
-int Data::getAno(){
-	return ano;
+int Invoice::getQuant(){
+	return quantidade;
 }
 
-void Data::setDia(int d){
-	dia = d;
-}
-void Data::setMes(int m){
-	mes = m;
-}
-void Data::setAno(int a){
-	ano = a;
+float Invoice::getPreco(){
+	return preco;
 }
 
-void Invoice::getInvoiceAmount(){
-    return p*q
+void Invoice::setNumero(int n){
+	numero = n;
+}
+void Invoice::setDescricao(string d){
+	descricao = d;
+}
+void Invoice::setQuant(int q){
+	quantidade = q;
 }
 
-void Data::avancarDia(){
-	if ((dia >= 1)&&(dia <=30))
-        dia++;
-	if ((mes >= 1)&& (dia == 31))
-		mes++;
-	if ((ano >=0) && (mes == 12))
-		ano++;
+void Invoice::setPreco(float p){
+	preco = p;
+}
+
+float Invoice::getInvoiceAmount(){
+    return quantidade*preco;
 
 }
+
+
